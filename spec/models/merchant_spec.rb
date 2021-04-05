fea5f1c9afc7e3940087c02ffbe92a30f0837048
Megan Gonzales <megan.campbell26@yahoo.com>
2021-04-05 17:10:21 -0600

    Add pagination and serialization

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe Merchant, type: :model do
   describe "Relationships" do
     it { should have_many :items }
   end
+
+  describe "scope" do
+    describe ".paginate" do
+      it "seperates into pages and limits merchant per page" do
+        merchant_1 = create(:merchant)
+        merchant_2 = create(:merchant)
+        merchant_3 = create(:merchant)
+        merchant_4 = create(:merchant)
+        merchant_5 = create(:merchant)
+        merchant_6 = create(:merchant)
+
+        expected = [merchant_3, merchant_4]
+        
+        expect(Merchant.paginate(page: 2, per_page: 2)).to eq(expected)
+      end
+    end
+  end
 end
--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -1,15 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants API" do
-  it "gets all merchants" do
+  it "gets all merchants limited to 20 unless optional query params passed" do
     create_list(:merchant, 50)
 
     get '/api/v1/merchants'
 
     expect(response).to be_success
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(50)
+    expect(merchants[:data].count).to eq(20)
+
+    get '/api/v1/merchants?per_page=45'
+
+    expect(response).to be_success
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(45)
   end
+
+  it "gets all merchants limited to 20 unless optional query params passed including page numbers" do
+    merchant_1 = create(:merchant)
+    merchant_2 = create(:merchant)
+    merchant_3 = create(:merchant)
+    merchant_4 = create(:merchant)
+    merchant_5 = create(:merchant)
+    merchant_6 = create(:merchant)
+    merchant_7 = create(:merchant)
+    merchant_8 = create(:merchant)
+    merchant_9 = create(:merchant)
+    merchant_10 = create(:merchant)
+
+    get '/api/v1/merchants?per_page=8'
+
+    expect(response).to be_success
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants[:data].count).to eq(8)
+
+    get '/api/v1/merchants?per_page=5&page=2'
+
+    expect(response).to be_success
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants).to be_a(Hash)
+    expect(merchants[:data]).to be_an(Array)
+    expect(merchants[:data][0][:id].to_i).to eq(merchant_6.id)
+    expect(merchants[:data][4][:id].to_i).to eq(merchant_10.id)
+  end
+  
+  
 end

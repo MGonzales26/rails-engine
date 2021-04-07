09c586126cc2f5bf27b55fbaf0d74351bb0d2942
Megan Gonzales <megan.campbell26@yahoo.com>
2021-04-07 09:25:49 -0600

    Add test for alphabetical order of merchant search

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,7 @@ class Merchant < ApplicationRecord
   has_many :items
 
   def self.find_merchant_by_name(name)
-    merchant = where('name ilike ?', "%#{name}%").first
+    merchant = where('name ilike ?', "%#{name}%").order(:name).first
 
   end
 end
